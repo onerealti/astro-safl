57530d44a2b77352c62b59e267c4f26713ee5cac
onerealti <murtazaofficial@proton.me>
2026-06-22 17:47:06 +0530

    ci: updated config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://onerealti.github.io',
+	base: 'astro-safl',
 	integrations: [
 		starlight({
 			title: 'My Docs',
